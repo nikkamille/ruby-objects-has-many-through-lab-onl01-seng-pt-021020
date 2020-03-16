53a4035fa7f8d32da549038b9f424da9a09bf540
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-16 22:33:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end
   
